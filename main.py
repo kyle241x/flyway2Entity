14ad79ef89582ea3357c59c3eed758340c9a6be5
kyle <835971987@qq.com>
2022-04-06 23:30:44 +0800

    [tp] feat: ui

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5 import QtWidgets
+
+from UI.UI import Ui_MainWindow
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(main_window)
+    main_window.show()
+    sys.exit(app.exec_())
